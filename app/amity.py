fed9c2510098e51788e9f19ca0d5b12db35b73a9
Angela Mutava <angiemutava@Angelas-MacBook-Pro.local>
2017-04-21 07:33:37 +0300

    Added the class skeleton

---
+++ app/amity.py
@@ -0,0 +1,36 @@
+from person import Person, Fellow, Staff
+from room import Room, Office, LivingSpace
+class Amity(object):
+
+	def add_person(self, name, employee, need_accomodation):
+		pass
+
+	def create_room(self, list_of_rooms, room_type):
+		pass
+
+	def allocate_employee(self, employee_name, room_name):
+		pass
+
+	def accomodate_fellow(self, fellow_name, space_name):
+		pass
+
+	def reallocate_employee(self, employee_name, space_name):
+		pass
+
+	def print_room_occupants(self):
+		pass
+
+	def print_allocated_rooms(self):
+		pass
+
+	def print_unallocated_room(self):
+		pass
+
+	def print_rooms(self):
+		pass 
+
+	def load_people(self, file_path):
+		pass											
+  
+
+         
