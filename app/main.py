4cdddf5fdc04c50e1489df942f9afce0c1cd9081
Angela Mutava <angiemutava@Angelas-MacBook-Pro.local>
2017-05-04 16:17:53 +0300

    Added the commands for amity

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ A brief description of what the application does.
 """
 import cmd
 from docopt import docopt, DocoptExit
-from app.amity import *
+from amity import *
 from pyfiglet import figlet_format
 from termcolor import cprint
 
@@ -34,48 +34,61 @@ def docopt_cmd(func):
 
 class Amity(cmd.Cmd):
 	intro = cprint(figlet_format("Amity", font="big"), "yellow", attrs=['bold'])
-	print "**********User Guide************"
-	print "    Command    Description     Parameter"
+	print ("**********User Guide************")
+	print ("    Command    Description     Parameter")
 	
-	print "type --help-- to view commands"
+	print ("type --help-- to view commands")
 	prompt = "<--Amity-->"
 
 	@docopt_cmd
-	def do_x(self, arg):
-		"""Usage: find <query> """
-		query = arg["<query>"]
-		#song_find(query)
+	def do_create_room(self, arg):
+		"""Usage: create_room <room_name> <room_type>"""
+		room_name = arg["<room_name>"]
+		room_type = arg["<room_type>"]
+		amity.create_room(room_name, room_type)
+
+	@docopt_cmd
+	def do_add_person(self, arg):
+		"""Usage: add_person <name> <employee_type> <need_accomodation>"""	
+		
 		
 
 	@docopt_cmd
-	def do_y(self, arg):
-		"""Usage: view <track_id>"""	
-		track_id = arg["<track_id>"]
+	def do_reallocate_employee(self, arg):
+		"""Usage: reallocate_employee <>"""
 		
 
 	@docopt_cmd
-	def do_z(self, arg):
-		"""Usage: clear
-		"""
+	def	do_print_room_occupants(self):
+		"""Usage: print_room_occupants"""
+		
 		
 
 	@docopt_cmd
-	def	do_A(self, arg):
-		"""Usage: save <track_id>"""
-		track_id = arg["<track_id>"]
+	def do_print_allocated_rooms(self):
+		"""Usage: print_allocated_rooms"""
 		
 
 	@docopt_cmd
-	def do_B(self, arg):
-		"""Usage: clear_song <track_id>"""
-		track_id = arg["<track_id>"]
+	def do_print_unallocated_rooms(self):
+		"""Usage: print_unallocated_rooms"""
+		
+
+	@docopt_cmd
+	def do_load_people(self):
+		"""Usage: load_people"""
+		
+
+	@docopt_cmd
+	def do_save_people(self, arg):
+		"""Usage: save_people"""			
 		
 
 	
 	def do_quit(self, arg):
 		"""Exits the application
 	    """
-		print "Welcome again!"
+		print ("Welcome again!")
 		exit()
 if __name__ == '__main__':
-	LyricsFinder().cmdloop()
+	Amity().cmdloop()
