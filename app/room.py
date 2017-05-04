a684840cff08a976f92f28c4bfbe3c11f77d07ab
Angela Mutava <angiemutava@Angelas-MacBook-Pro.local>
2017-05-04 10:34:43 +0300

    Added the room occupants variablet to the subclasses

--- app/room.py
+++ app/room.py
@@ -1,41 +1,53 @@
 import random
+
+
 class Room(object):
-	def __init__(self, room_name,  room_capacity ):
+	def __init__(self, room_name, room_capacity):
 		self.room_name = room_name
-		
+
 		self.room_capacity = room_capacity
 		self.room_occupants = []
 
-	def allocate_space(self, person):
-		if self.is_full == True:
-			return "The room is full try other rooms."
-		else:
-			room_occupants.append(person)
-					
+	def get_room_name(self):
+		return self.room_name
+
+	def get_room_capacity(self):
+		return self.room_capacity
+
 
 class Office(Room):
 	def __init__(self, room_name):
-		super(Office, self).__init__(room_name,room_capacity = 6)
+		super(Office, self).__init__(room_name, room_capacity = 6)
+		self.room_occupants = []
 
 	def allocate_office_space(self, person):
 			pass
 
 	def is_full(self):
-		if len(self.room_capacity) == self.room_capacity:
-			return True		
-		return len(self.room_capacity)
+		if len(self.room_occupants) == self.room_capacity:
+			return True
+
+	#def get_room_name(self):
+		#return self.room_name
+	
+	def get_room_capacity(self):
+		return self.room_capacity					
 		
    
 
 class LivingSpace(Room):
-    def __init__(self, room_name):
-		super(LivingSpace, self).__init__(room_name, room_capacity = 4)
-
-	def allocate_living_space(self, person):
-		pass
+	def __init__(self, room_name):
+		super(LivingSpace, self).__init__(room_name, room_capacity = 6)
+		self.room_occupants = []
 
 	def is_full(self):
-		if len(self.room_capacity) == self.room_capacity:
+		if len(self.room_occupants) == self.room_capacity:
 			return True		
-		return len(self.room_capacity)		
+		return len(self.room_capacity)
+
+	def get_room_name(self):
+		return self.room_name
+
+	def get_room_capacity(self):
+		return self.room_capacity				
 		
